3f1d2ec2b2bc0179853e9741ed875535765092f0
raszti <raszti@gmail.com>
2017-07-15 14:42:22 -0700

    Added intents
    Singlescreen app with intents

--- app/src/main/java/com/example/raszti/intergalacticresort/MainActivity.java
+++ app/src/main/java/com/example/raszti/intergalacticresort/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.raszti.intergalacticresort;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.button;
+import static android.R.id.message;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void moveIn (View v) {
+        String uriText =
+                "mailto:resorts@intergalactic.com" +
+                        "?subject=" + Uri.encode("MOVING IN!") +
+                        "&body=" + Uri.encode("I can't wait to board the shuttle!");
+
+        Uri uri = Uri.parse(uriText);
+
+        Intent sendIntent = new Intent(Intent.ACTION_SENDTO);
+        sendIntent.setData(uri);
+        startActivity(Intent.createChooser(sendIntent, "Send email"));
+    }
+
+    public void callNow (View v) {
+        String phone = "+1 358 223 6277";
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+        startActivity(intent);
     }
 }
